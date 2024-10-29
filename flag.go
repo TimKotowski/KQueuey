9aa35506585b84989c7eaa866ee5d2620f615ca2
Tim Kotowski <tkotowski7@gmail.com>
2024-10-29 18:04:24 -0500

    word clean up

--- flag.go
+++ flag.go
@@ -11,8 +11,8 @@ type FlagOpts struct {
 
 func (f *FlagOpts) RegisterFlags() {
 	flag.StringVar(&f.ConfigPath, "config", "/usr/local/etc/", "path to configuration file, default: /usr/local/etc/")
-	flag.StringVar(&f.Logging.Level, "logLevel", "info", "provides log level: debug, info, trace, error, warn, fatal")
-	flag.StringVar(&f.Logging.Format, "logFormat", "json", "provides what format logs should be presented as")
+	flag.StringVar(&f.Logging.Level, "logLevel", "info", "provides log level: debug, info, warn, error, default: info")
+	flag.StringVar(&f.Logging.Format, "logFormat", "json", "logger format options")
 }
 
 func (f *FlagOpts) Parse() {
